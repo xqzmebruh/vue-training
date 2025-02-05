c90843893587cf93f929d6fcd79a9a258421650b
shumakov_kostya <shumakov@onr-kursk.ru>
2025-02-05 16:14:36 +0300

    dual binding

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,101 @@
 <template>
- <div class="userform-wrapper">
+    <div class="userform-wrapper">
         <div class="userform">
-    <div class="textbox"><strong>Имя</strong>
-    <form action="">
-        <input class="inp" type="text">
-    </form></div>
-    <div class="textbox"><strong>Фамилия</strong>
-    <form action="">
-        <input class="inp" type="text">
-    </form></div>
-    <div class="textbox"><strong>Отчество</strong>
-    <form action="">
-        <input class="inp" type="text">
-    </form></div>
-    <div class="textbox"><strong>Дата рождения</strong>
-    <form action="">
-        <input class="inp" type="text">
-    </form></div>
-    <button class="btn">Создать</button>
-    
-    <div></div>
+            <div class="textbox"><strong>Имя</strong>
+            
+                <input
+                @input="userName = $event.target.value" 
+                v-bind:value="userName" 
+                class="inp" 
+                type="text"
+                >
+            
+            </div>
+            <div class="textbox"><strong>Фамилия</strong>
+            
+                <input 
+                @input="userSurname = $event.target.value"
+                v-bind:value="userSurname" 
+                class="inp" 
+                type="text"
+                >
+            
+            </div>
+            <div class="textbox"><strong>Отчество</strong>
+            
+                <input 
+                @input="userLastname = $event.target.value"
+                v-bind:value="userLastname" 
+                class="inp" 
+                type="text"
+                >
+            
+            </div>
+            <div class="textbox"><strong>Дата рождения</strong>
+            
+                <input 
+                @input="dateOfBirth = $event.target.value"
+                v-bind:value="dateOfBirth" 
+                class="inp" 
+                type="text"
+                >
+            
+            </div>
 
-</div>
-</div>
+            <button class="btn" @click="createUser">Создать</button>
+        </div>
+    </div>
+
+    <div class="user" v-for="user in users" :key="user.id">
+        <div><strong>Имя: </strong> {{ user.userName }}</div>
+        <div><strong>Фамилия: </strong> {{ user.userSurname }}</div>
+        <div><strong>Отчество: </strong> {{ user.userLastname }}</div>
+        <div><strong>Дата рождения: </strong> {{ user.dateOfBirth }}</div>
+    </div>
+
+
+
 </template>
 
 <script>
-
+export default{
+ data() {
+     return{
+         users :[
+             {id: 1, userName: 'test', userSurname: 'testov', userLastname: 'testovich', dateOfBirth: '00.00.1989'}
+         ],
+         userName: '',
+         userSurname: '',
+         userLastname: '',
+         dateOfBirth: '',
+     }
+ },
+  methods: {
+        createUser(){
+            const newUser = {
+                id: Date.now(),
+                userName: this.userName,
+                userSurname: this.userSurname,
+                userLastname: this.userLastname,
+                dateOfBirth: this.dateOfBirth
+            }
+            this.users.push(newUser)
+            this.userName = '';
+            this.userSurname = '';
+            this.userLastname = '';
+            this.dateOfBirth = '';
+        },
+       
+     }
+}
 </script>
 
 <style>
 .textbox {
     margin-top: 3px;
     margin-bottom: 3px;
-    
+    display: flex;
+    flex-direction: column;
 }
 .userform-wrapper {
     display: flex;
@@ -58,5 +120,11 @@
     border: 1px solid teal;
     padding-bottom: 3px;
     padding-top: 3px;
+}
+
+.user{
+    padding: 10px;
+    margin-top: 10px;
+    border: 1px solid teal;
 }
 </style>
